14eec4bb25dc11ae1bf3efffbb6f8d2ab8ee7e75
JacquelineMG <j.gilchrist@outlook.com>
2023-04-19 11:41:51 -0700

    assertEqual function compares two values and prints out a colourful successful or failure message

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(String.fromCodePoint(0x00002705) + " Assertion Passed: " + actual + " === " + expected + " " + String.fromCodePoint(0x00002705));
+  } else {
+    console.log(String.fromCodePoint(0x0000274C) + " Assertion Failed: " + actual + " !== " + expected + " "  + String.fromCodePoint(0x0000274C));
+  }
+};
+
+assertEqual("lighthouse labs", "lighthouse labs");
+assertEqual(1, 2);

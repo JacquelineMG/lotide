d0246bc9c4df14fe35417860ccd6d1f714b8d64d
JacquelineMG <j.gilchrist@outlook.com>
2023-04-19 13:24:49 -0700

    Improved assertEqual using Template Literals instead of concatenate values

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(String.fromCodePoint(0x00002705) + " Assertion Passed: " + actual + " === " + expected + " " + String.fromCodePoint(0x00002705));
+    console.log(`${String.fromCodePoint(0x00002705)} Assertion Passed: ${actual} === ${expected} ${String.fromCodePoint(0x00002705)}`);
   } else {
-    console.log(String.fromCodePoint(0x0000274C) + " Assertion Failed: " + actual + " !== " + expected + " "  + String.fromCodePoint(0x0000274C));
+    console.log(`${String.fromCodePoint(0x0000274C)} Assertion Failed: ${actual} !== ${expected} ${String.fromCodePoint(0x0000274C)}`);
   }
 };
 

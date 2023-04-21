bfeb9ed5dbbf61453e1747c5f22abe90dfc261a2
JacquelineMG <j.gilchrist@outlook.com>
2023-04-21 10:23:47 -0700

    add eqArrays.js

---
+++ eqArrays.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`${String.fromCodePoint(0x00002705)} Assertion Passed: ${actual} === ${expected} ${String.fromCodePoint(0x00002705)}`);
+  } else {
+    console.log(`${String.fromCodePoint(0x0000274C)} Assertion Failed: ${actual} !== ${expected} ${String.fromCodePoint(0x0000274C)}`);
+  }
+};
+
+
+const eqArrays = function(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1.length !== arr2.length) {
+      return false;
+    }
+    else if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  } return true
+};
+
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3]))// => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1]))// => false
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3",])) // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, "3"], [1, 2, 3]), false);

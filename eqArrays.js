1c4a39441e8303add9c59f8bb31301b4f80a29e6
JacquelineMG <j.gilchrist@outlook.com>
2023-05-12 11:41:58 -0700

    refactor eqArrays moving for loop below first if statement

--- eqArrays.js
+++ eqArrays.js
@@ -1,16 +1,18 @@
 // FUNCTION DESCRIPTION:
-// eqArrays assesses whether two arrays are a strictly equal and returns a true or false boolean
+// eqArrays assesses whether two single-level arrays are a strictly equal and returns a true or false boolean
 
-//INPUT: 2 arrays
+//INPUT: 2 single-level arrays
 //OUTPUT: returns a boolean  
 
 
 const eqArrays = function(arr1, arr2) {
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1.length !== arr2.length) {
-      return false;
-    } else if (arr1[i] !== arr2[i]) {
-      return false;
+  if (arr1.length !== arr2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
     }
   } return true;
 };
